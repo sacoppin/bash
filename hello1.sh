9cd897379bfb22a09fc8fba3e275ce2277960581
sacoppin <sacoppin@hotmail.com>
2022-04-21 10:07:37 +0200

    filemaker

---
+++ hello1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a comment!
+#  chmod u+x hello1.sh
+#  ./hello1.sh
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
