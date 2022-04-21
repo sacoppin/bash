895435465b65a7b62ccb3cc155873a53f8d7fb11
sacoppin <sacoppin@hotmail.com>
2022-04-21 10:32:01 +0200

    path

---
+++ path.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#  chmod u+x path.sh
+#  ./path.sh
+#Write a shell script receiving a path as argument, depending on if it's a file or a directory, display or list its content.
+#You can complexify the script by only displaying file containing text (ex: .txt, .js) and returning an error for anything else (ex: .mp3, .pdf).
+
+echo Where are we?
+pwd
+echo Ok what files do you have?
+echo Give me path to files please.
+
+
